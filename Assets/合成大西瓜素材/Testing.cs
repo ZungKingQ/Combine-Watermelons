f1884db7b918aae83243b398561af7e07aef3d63
ZungKingQ <Zhongjh18819344688@163.com>
2023-01-31 15:38:29 +0800

    Initial commit

---
+++ Assets/合成大西瓜素材/Testing.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testing : MonoBehaviour
+{
+    private int a = 5;
+    public int b
+    {
+        get => a;
+        set { b = value;}
+    }
+    void Start()
+    {
+        
+    }
+    void Plus(int value)
+    {
+        value += 2;
+        print(value);
+    }
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Plus(b);
+        }
+    }
+}
